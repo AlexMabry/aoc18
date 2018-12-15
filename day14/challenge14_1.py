4cde075f6fd70370509fdb117d4ed9ff4d08cd76
Alex Mabry <alex.mabry@synapse-wireless.com>
2018-12-15 10:58:16 -0600

    Day 14

--- day14/challenge14_1.py
+++ day14/challenge14_1.py
@@ -4,8 +4,7 @@ elf1 = 0
 elf2 = 1
 
 while len(circle) < rounds + 10:
-    new_recipe = [int(c) for c in str(circle[elf1] + circle[elf2])]
-    circle.extend(new_recipe)
+    circle.extend([int(c) for c in str(circle[elf1] + circle[elf2])])
     elf1 = (elf1 + circle[elf1] + 1) % len(circle)
     elf2 = (elf2 + circle[elf2] + 1) % len(circle)
 
--- day14/challenge14_2.py
+++ day14/challenge14_2.py
@@ -6,9 +6,8 @@ elf2 = 1
 found = -1
 while found < 0:
     circle += str(int(circle[elf1]) + int(circle[elf2]))
-    length = len(circle)
-    elf1 = (elf1 + int(circle[elf1]) + 1) % length
-    elf2 = (elf2 + int(circle[elf2]) + 1) % length
-    found = circle.find(puzzle_input, length-len(puzzle_input)-2, length)
+    elf1 = (elf1 + int(circle[elf1]) + 1) % len(circle)
+    elf2 = (elf2 + int(circle[elf2]) + 1) % len(circle)
+    found = circle.find(puzzle_input, len(circle)-len(puzzle_input)-2, len(circle))
 
 print(found)
